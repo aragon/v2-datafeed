92d75a6cdf0d2cc02200da686e59c59cb3b6f0e2
Ramon Canales <ramon@aragon.org>
2021-05-11 09:06:43 +0100

    trying to fix docker build

--- .github/scripts/docker-build.sh
+++ .github/scripts/docker-build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # use previously built image for cache if possible
-REPO="$1"
-GITHUB_SHA="$2"
-docker pull $REPO:latest || true
-docker pull $REPO:rolling || true
-docker build . -t $REPO:${GITHUB_SHA} -t $REPO:rolling --cache-from $REPO:rolling --cache-from $REPO:latest
+DOCKERFILE_PATH="$1"
+REPO="$2"
+GITHUB_SHA="$3"
+# use previously built image for cache if possible
+docker pull $REPO:latest  true
+docker pull $REPO:rolling  true
+docker build $DOCKERFILE_PATH -t $REPO:${GITHUB_SHA} -t $REPO:rolling --cache-from $REPO:rolling --cache-from $REPO:latest
 docker push $REPO:${GITHUB_SHA}
 docker push $REPO:rolling
