df4d61b2d6ec7325a15eaabca5ee68072dd9a053
Ramon Canales <ramon@aragon.org>
2021-05-11 08:45:05 +0100

    changing / to :

--- .github/scripts/docker-build.sh
+++ .github/scripts/docker-build.sh
@@ -2,8 +2,8 @@
 # use previously built image for cache if possible
 REPO="$1"
 GITHUB_SHA="$2"
-docker pull $REPO/latest || true
-docker pull $REPO/rolling || true
-docker build . -t $REPO/${GITHUB_SHA} -t $REPO/rolling --cache-from $REPO/rolling --cache-from $REPO/latest
-docker push $REPO/${GITHUB_SHA}
-docker push $REPO/rolling
+docker pull $REPO:latest || true
+docker pull $REPO:rolling || true
+docker build . -t $REPO:${GITHUB_SHA} -t $REPO:rolling --cache-from $REPO:rolling --cache-from $REPO:latest
+docker push $REPO:${GITHUB_SHA}
+docker push $REPO:rolling
