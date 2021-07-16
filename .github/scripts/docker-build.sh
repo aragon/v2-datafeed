dab0641593c3c6b9bf9bc79d8544218326b3e0b8
Mathias Scherer <mathias@aragon.org>
2021-07-16 12:24:13 +0200

    fixes docker build and release ci pipeline

--- .github/scripts/docker-build.sh
+++ .github/scripts/docker-build.sh
@@ -3,6 +3,6 @@
 DOCKERFILE_PATH="$1"
 REPO="$2"
 GITHUB_SHA="$3"
-docker build $DOCKERFILE_PATH -t $REPO:${GITHUB_SHA} -t $REPO:rolling --cache-from $REPO:rolling --cache-from $REPO:latest
+docker build $DOCKERFILE_PATH -t $REPO:${GITHUB_SHA} -t $REPO:rolling
 docker push $REPO:${GITHUB_SHA}
 docker push $REPO:rolling
